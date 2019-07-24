dcdaff0a76072a54338dffe55cb3407f1e79de77
Ícaro Pires <icaropsa@gmail.com>
2019-07-24 16:11:22 -0300

    Check for supported PIDS on running

--- collect/collect.go
+++ collect/collect.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -41,27 +42,27 @@ var readingWritingMux sync.Mutex
 func main() {
 	// Consult https://en.wikipedia.org/wiki/OBD-II_PIDs for basic PIDS
 	parameters := map[string]*Parameter{
-		"vehicleSpeed": GetParameter("0D", func(out []byte) float64 {
-			return float64(out[0]) // km/h
+		"engineLoad": GetParameter("04", func(out []byte) float64 {
+			return 100 * float64(out[0]) / 255 // %
+		}),
+		"engineCoolanteTemperature": GetParameter("05", func(out []byte) float64 {
+			return float64(out[0]) - 40 // Cº
 		}),
 		"engineRPM": GetParameter("0C", func(out []byte) float64 {
-			return 256*float64(out[0]) + float64(out[1])/4 // RPM
+			return (256*float64(out[0]) + float64(out[1])) / 4 // RPM
 		}),
-		"absoluteBarometricPressure": GetParameter("33", func(out []byte) float64 {
-			return float64(out[0]) // kPa
-		}),
-		"throttlePosition": GetParameter("11", func(out []byte) float64 {
-			return 100 * float64(out[0]) / 255 // %
+		"vehicleSpeed": GetParameter("0D", func(out []byte) float64 {
+			return float64(out[0]) // km/h
 		}),
 		"traveledWithMalfunction": GetParameter("21", func(out []byte) float64 {
 			return 256*float64(out[0]) + float64(out[1]) // Km
 		}),
-		"runtimeSinceEngineStart ": GetParameter("1F", func(out []byte) float64 {
-			return float64(out[0])*256 + float64(out[1]) // seconds
-		}),
-		"ambientAirTemperature": GetParameter("46", func(out []byte) float64 {
-			return float64(out[0]) - 40 // Cº
-		}),
+		//"runtimeSinceEngineStart ": GetParameter("1F", func(out []byte) float64 {
+		//	return float64(out[0])*256 + float64(out[1]) // seconds
+		//}),
+		//"ambientAirTemperature": GetParameter("46", func(out []byte) float64 {
+		//	return float64(out[0]) - 40 // Cº
+		//}),
 	}
 
 	setUpPort()
@@ -81,12 +82,21 @@ func main() {
 
 	var wg sync.WaitGroup
 	wg.Add(1)
+
+	supportedPids := prepareElm327()
 	for name, parameter := range parameters {
 		go func(n string, p Parameter) {
 			count := 0
 			samples := make([]float64, samplesAmount)
 
 			for {
+				pidBytes, err := hex.DecodeString(p.pid)
+				pidInt := int(pidBytes[0])
+				if !isPidSupported(supportedPids, pidInt) {
+					log.Printf("PID: 0x%02X is not supported by this vehicle!", pidInt)
+					break
+				}
+
 				out, err := p.collectData()
 				if err == nil {
 					samples[count] = out
@@ -133,21 +143,70 @@ func setUpPort() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
+func prepareElm327() []int {
 	port.Flush()
 
 	log.Println("Reseting ELM327...")
 	submitCommand([]byte("ATZ\r"), 1000)
 
+	pids := getSupportedPids()
+	printSupportedPids(pids)
+
+	return pids
+}
+
+func isPidSupported(pids []int, pid int) bool {
+	idx := sort.SearchInts(pids, pid)
+
+	if idx == len(pids) {
+		return false
+	}
+
+	if pids[idx] == pid {
+		return true
+	}
+
+	return false
+}
+
+func getSupportedPids() []int {
 	log.Println("Setting searching for protocol...")
 	submitCommand([]byte("ATSP0\r"), 100)
 
 	port.Flush()
-	log.Println("Searching for protocols and supported pids")
+	log.Println("Searching for protocols and supported pids (1-20)...")
 	buf, _ := submitCommand([]byte("0100\r"), 7000)
 	data := getDataBytes(buf, "41", "00")
-	pids := getSupportedPids(data)
-	printSupportedPids(pids)
+	pids := parseSupportedPids(data, 0x01)
+
+	supportsNext := true
+	for pidSupport := 0x20; supportsNext; pidSupport += 0x20 {
+		supportsNext = false
+
+		for i := len(pids) - 1; i >= 0; i-- {
+			if pids[i] == pidSupport {
+				supportsNext = true
+				break
+			} else if pids[i] < pidSupport {
+				break
+			}
+		}
+
+		if supportsNext {
+			log.Printf("Searching for supported pids (%02X-%02X)...", pidSupport+1, pidSupport+0x20)
+
+			pidSupportStr := fmt.Sprintf("%02X", pidSupport)
+			command := "01" + pidSupportStr + "\r"
+
+			buf, _ := submitCommand([]byte(command), 200)
+			data := getDataBytes(buf, "41", pidSupportStr)
+			pids = append(pids, parseSupportedPids(data, pidSupport+1)...)
+		}
+	}
+
+	return pids
 }
 
 func printSupportedPids(pids []int) {
@@ -159,12 +218,12 @@ func printSupportedPids(pids []int) {
 	fmt.Println("Supported PIDS by the vehicle: " + strings.Join(pidsStr, ", "))
 }
 
-func getSupportedPids(data []byte) []int {
+func parseSupportedPids(data []byte, offset int) []int {
 	pids := make([]int, 0)
 	for i, b := range data {
 		for j := 7; j >= 0; j-- {
 			if b&(1<<uint(j)) != 0 {
-				pids = append(pids, 1+(i*8)+int(7-j))
+				pids = append(pids, offset+(i*8)+int(7-j))
 			}
 		}
 	}
@@ -269,7 +328,7 @@ func getDataBytes(in []byte, expectedService string, pid string) []byte {
 
 	endOfData := strings.Index(octectsStr, "\r")
 	if endOfData == -1 {
-		endOfData = len(octectsStr) - 1
+		endOfData = len(octectsStr)
 	}
 	octectsStr = octectsStr[:endOfData]
 
